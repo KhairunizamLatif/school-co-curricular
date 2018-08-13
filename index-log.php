e2f9754c71c6deb4d332c7d82338309d855a29eb
Khairunizam Latif <khairunizam.latif@terang.asia>
2018-08-13 12:48:13 +0800

    initial checkin

---
+++ index-log.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+if(!isset($_SESSION['id']))
+{
+    $_SESSION['id']=$_POST['id'];
+}
+$id=$_SESSION['id'];
+?>
+<?php
+include"config.php";
+if($dbconn !== FALSE){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $sel="SELECT * from code where code='$code' and nama_cikgu='$username' ";
+    $q=mysql_query($sel,$dbconn);
+    $bilRow=mysql_num_rows($q);
+    if($bilRow>0)
+    {
+        $row=mysql_fetch_array($q);
+        $_SESSION['id']=$row[0];
+        $_SESSION['id_code']=$row[1];
+        $_SESSION['siapa']=$row[2];
+        $_SESSION['username']=$row[3];
+        $_SESSION['password']=$row[4];
+        $_SESSION['keyword']=$row[5];
+        if($row[1]==1)
+        {
+            header("Location:#");
+        }
+        if($row[1]==2)
+        {
+            header("Location:#");
+        }
+        if($row[1]==3)
+        {
+            header("Location:#");
+        }
+    }
+
+    else
+    {
+        $_SESSION['id']=$id;
+        echo "<script type='text/javascript'>";
+        echo "alert('Wrong Username & Password.')";
+        echo "</script>";
+        echo "<meta http-equiv='refresh' content='0;URL=insert_code.php'>";
+    }
+}
+
+?>
---
+++ js/dashboard.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $('[data-toggle="offcanvas"]').click(function(){
+        $("#navigation").toggleClass("hidden-xs");
+    });
+});
+
+
+<!------------------------form step----------------------->
+
+$(document).ready(function () {
+
+    var navListItems = $('div.setup-panel div a'),
+        allWells = $('.setup-content'),
+        allNextBtn = $('.nextBtn');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('div.setup-panel div a.btn-primary').trigger('click');
+});
+
+
+<!-- table filter -->
+
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+
+<!-- modal -->
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
